81e08a3b125423e0ef86a6438947e7451708a9ba
Alexey Gulev <me@agulev.com>
2021-07-04 16:35:49 +0200

    ios initial implementation

--- defunityads/src/unityads_callback_private.h
+++ defunityads/src/unityads_callback_private.h
@@ -10,41 +10,45 @@ namespace dmUnityAds {
 
 enum MessageId
 {
-    MSG_INIT=                    1,
-    MSG_SHOW =                   2,
-    MSG_LOAD =                   3,
-    MSG_BANNER =                 4,
-    MSG_IDFA =                   5
+    MSG_INIT=                        1,
+    MSG_SHOW =                       2,
+    MSG_LOAD =                       3,
+    MSG_BANNER =                     4,
+    MSG_IDFA =                       5
 };
 
 enum MessageEvent
 {
-    EVENT_COMPLETED =            1,
-    EVENT_SDK_ERROR =            2,
-    EVENT_JSON_ERROR =           3,
-    EVENT_CLICKED =              4,
-    EVENT_START =                5,
-    EVENT_LOADED =               6,
-    EVENT_LEFT_APPLICATION =     7,
-    EVENT_SKIPPED =              8,
-    EVENT_NOT_SUPPORTED =        9
+    EVENT_COMPLETED =                1,
+    EVENT_SDK_ERROR =                2,
+    EVENT_JSON_ERROR =               3,
+    EVENT_CLICKED =                  4,
+    EVENT_START =                    5,
+    EVENT_LOADED =                   6,
+    EVENT_LEFT_APPLICATION =         7,
+    EVENT_SKIPPED =                  8,
+    EVENT_NOT_SUPPORTED =            9,
+    EVENT_STATUS_AUTORIZED =        10,
+    EVENT_STATUS_DENIED =           11,
+    EVENT_STATUS_NOT_DETERMINED =   12,
+    EVENT_STATUS_RESTRICTED =       13
 };
 
 enum Errors
 {
-    ERROR_INTERNAL =             1,
-    ERROR_INVALID_ARGUMENT =     2,
-    ERROR_NOT_INITIALIZED =      3,
-    ERROR_NOT_READY =            4,
-    ERROR_VIDEO_PLAYER =         5,
-    ERROR_NO_CONNECTION =        6,
-    ERROR_ALREADY_SHOWING =      7,
-    ERROR_NO_FILL =              8,
-    ERROR_TIMEOUT =              9,
-    ERROR_UNKNOWN =             10,
-    ERROR_NATIVE =              11,
-    ERROR_WEBVIEW =             12,
-    ERROR_AD_BLOCKER_DETECTED = 13
+    ERROR_INTERNAL =                 1,
+    ERROR_INVALID_ARGUMENT =         2,
+    ERROR_NOT_INITIALIZED =          3,
+    ERROR_NOT_READY =                4,
+    ERROR_VIDEO_PLAYER =             5,
+    ERROR_NO_CONNECTION =            6,
+    ERROR_ALREADY_SHOWING =          7,
+    ERROR_NO_FILL =                  8,
+    ERROR_TIMEOUT =                  9,
+    ERROR_UNKNOWN =                 10,
+    ERROR_NATIVE =                  11,
+    ERROR_WEBVIEW =                 12,
+    ERROR_AD_BLOCKER_DETECTED =     13
 };
 
 struct CallbackData
--- defunityads/src/unityads_ios.mm
+++ defunityads/src/unityads_ios.mm
@@ -1,139 +1,177 @@
 #if defined(DM_PLATFORM_IOS)
-#include "../private_DefUnityAds.h"
-#include "../private_DefUnityCallback.h"
+#include "unityads_callback_private.h"
+#include "unityads_private.h"
 
 #include <UIKit/UIKit.h>
 #include <UnityAds/UnityAds.h>
 
+#if __has_include(<AppTrackingTransparency/ATTrackingManager.h>)
+#import <AppTrackingTransparency/ATTrackingManager.h>
+#endif
+
 @interface DefUnityAdsDelegate : NSObject<UnityAdsDelegate>
 @end
-
-@implementation DefUnityAdsDelegate
-
--(void)unityAdsReady:(NSString *)placementId {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_IS_READY,(char*)"placementId", (char*)[placementId UTF8String], NULL, 0);
-}
-
--(void)unityAdsDidStart:(NSString *)placementId {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_DID_START,(char*)"placementId", (char*)[placementId UTF8String], NULL, 0);
-}
-
--(void)unityAdsDidError:(UnityAdsError)error withMessage:(NSString *)message {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_DID_ERROR,(char*)"message", (char*)[message UTF8String], (char*)"error", (int)error);
-}
-
--(void)unityAdsDidFinish:(NSString *)placementId withFinishState:(UnityAdsFinishState)state {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_DID_FINISH,(char*)"placementId", (char*)[placementId UTF8String], (char*)"state", (int)state);
-}
-
-@end
-
-//Initialization
 
 @interface DefUnityAdsInitializationDelegate : NSObject<UnityAdsInitializationDelegate>
 @end
 
-@implementation DefUnityAdsInitializationDelegate
-
--(void)initializationComplete {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_INITIALIZED, NULL, NULL, NULL, 0);
-}
-
--(void)initializationFailed:(UnityAdsInitializationError)error withMessage:(NSString *)message {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_INIT_ERROR,(char*)"message", (char*)[message UTF8String], (char*)"error", (int)error);
-}
-
-@end
-
-//Banner:
-static UADSBannerView *gDefVideoAdsBannerView;
-
 @interface DefUnityAdsBannerDelegate: NSObject<UADSBannerViewDelegate>
 @end
 
-@implementation DefUnityAdsBannerDelegate
-- (void)bannerViewDidLoad:(UADSBannerView *)bannerView {
-    gDefVideoAdsBannerView = bannerView;
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_BANNER,(char*)"placementId", (char*)[bannerView.placementId UTF8String], (char*)"event", (int)dmUnityAds::BANNER_EVENT_DID_LOAD);
-}
+@interface DefUnityShowDelegate: NSObject<UnityAdsShowDelegate>
+@end
 
-- (void)bannerViewDidClick:(UADSBannerView *)bannerView {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_BANNER,(char*)"placementId", (char*)[bannerView.placementId UTF8String], (char*)"event", (int)dmUnityAds::BANNER_EVENT_DID_CLICK);
-}
-
-- (void)bannerViewDidLeaveApplication:(UADSBannerView *)bannerView {
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_BANNER,(char*)"placementId", (char*)[bannerView.placementId UTF8String], (char*)"event", (int)dmUnityAds::BANNER_EVENT_DID_LEAVE_APP);
-}
-
-- (void)bannerViewDidError:(UADSBannerView *)bannerView error:(UADSBannerError *)error{
-    dmUnityAds::AddToQueue((int)dmUnityAds::TYPE_BANNER_ERROR,(char*)"message", (char*)[[error localizedDescription] UTF8String], (char*)"error", (int)error.code);
-}
+@interface DefUnityAdsLoadDelegate: NSObject<UnityAdsLoadDelegate>
 @end
 
 namespace dmUnityAds {
 
 static UIViewController *uiViewController;
+
+void SendSimpleMessage(MessageId msg, id obj) {
+    NSError* error;
+    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:obj options:(NSJSONWritingOptions)0 error:&error];
+    if (jsonData)
+    {
+        NSString* nsstring = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        AddToQueueCallback(msg, (const char*)[nsstring UTF8String]);
+        [nsstring release];
+    }
+    else
+    {
+        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+        [dict setObject:error.localizedDescription forKey:@"error"];
+        [dict setObject:[NSNumber numberWithInt:EVENT_JSON_ERROR] forKey:@"event"];
+        NSError* error2;
+        NSData* errorJsonData = [NSJSONSerialization dataWithJSONObject:dict options:(NSJSONWritingOptions)0 error:&error2];
+        if (errorJsonData)
+        {
+            NSString* nsstringError = [[NSString alloc] initWithData:errorJsonData encoding:NSUTF8StringEncoding];
+            AddToQueueCallback(msg, (const char*)[nsstringError UTF8String]);
+            [nsstringError release];
+        }
+        else
+        {
+            AddToQueueCallback(msg, [[NSString stringWithFormat:@"{ \"error\": \"Error while converting simple message to JSON.\", \"event\": %d }", EVENT_JSON_ERROR] UTF8String]);
+        }
+    }
+}
+
+void SendSimpleMessage(MessageId msg, MessageEvent event) {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
+    SendSimpleMessage(msg, dict);
+}
+
+void SendSimpleMessage(MessageId msg, MessageEvent event, NSString *key_2, NSString *value_2) {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
+    [dict setObject:value_2 forKey:key_2];
+    SendSimpleMessage(msg, dict);
+}
+
+void SendSimpleMessage(MessageId msg, MessageEvent event, NSString *key_2, int value_2, NSString *key_3, NSString *value_3) {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject:[NSNumber numberWithInt:event] forKey:@"event"];
+    [dict setObject:[NSNumber numberWithInt:value_2] forKey:key_2];
+    [dict setObject:value_3 forKey:key_3];
+    SendSimpleMessage(msg, dict);
+}
     
-void initialize(const char*game_id, bool is_debug) {
+void Initialize(const char*game_id, bool is_debug, bool enablePerPlacementLoad) {
     NSString* gameId = [NSString stringWithUTF8String:game_id];
-    DefUnityAdsDelegate* unityAds = [[DefUnityAdsDelegate alloc] init];
-    DefUnityAdsInitializationDelegate* unityAdsInit = [[DefUnityAdsInitializationDelegate alloc] init];
-    [UnityAds initialize:gameId testMode:is_debug ? YES : NO initializationDelegate:unityAdsInit];
-    [UnityAds addDelegate:unityAds];
+    DefUnityAdsInitializationDelegate* unityAdsInitDelegate = [[DefUnityAdsInitializationDelegate alloc] init];
+    [UnityAds initialize:gameId testMode:is_debug ? YES : NO initializationDelegate:unityAdsInitDelegate 
+    enablePerPlacementLoad:enablePerPlacementLoad ? YES : NO];
+    if (!enablePerPlacementLoad) {
+        DefUnityAdsDelegate* unityAdsDelegate = [[DefUnityAdsDelegate alloc] init];
+        [UnityAds addDelegate:unityAdsDelegate];
+    }
     
     UIWindow* window = dmGraphics::GetNativeiOSUIWindow();
     uiViewController = window.rootViewController;
 }
 
-void show(char* placementId) {
-    if ((placementId != NULL) && (placementId[0] == '\0')) {
-        [UnityAds show:uiViewController];
+void RequestIDFA() {
+    if (@available(iOS 14, *))
+    {
+        [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+            switch (status) {
+                case ATTrackingManagerAuthorizationStatusAuthorized:
+                    SendSimpleMessage(MSG_IDFA, EVENT_STATUS_AUTORIZED);
+                    break;
+                case ATTrackingManagerAuthorizationStatusDenied:
+                    SendSimpleMessage(MSG_IDFA, EVENT_STATUS_DENIED);
+                    break;
+                case ATTrackingManagerAuthorizationStatusNotDetermined:
+                    SendSimpleMessage(MSG_IDFA, EVENT_STATUS_NOT_DETERMINED);
+                    break;
+                case ATTrackingManagerAuthorizationStatusRestricted:
+                    SendSimpleMessage(MSG_IDFA, EVENT_STATUS_RESTRICTED);
+                    break;
+            }
+        }];
     }
-    else {
-        NSString* placementId_s = [NSString stringWithUTF8String:placementId];
-        [UnityAds show:uiViewController placementId:placementId_s];
+    else
+    {
+        SendSimpleMessage(MSG_IDFA, EVENT_NOT_SUPPORTED);
     }
 }
 
-void setDebugMode(bool is_debug) {
+static DefUnityAdsLoadDelegate* unityLoadDelegate;
+
+void Load(char* placementId) {
+    NSString* placementId_s = [NSString stringWithUTF8String:placementId];
+    if (!unityLoadDelegate) {
+        unityLoadDelegate = [[DefUnityAdsLoadDelegate alloc] init];
+    }
+    [UnityAds load:uiViewController placementId:placementId_s loadDelegate:unityLoadDelegate];
+}
+
+static DefUnityShowDelegate* unityShowDelegate;
+
+void Show(char* placementId) {
+    NSString* placementId_s = [NSString stringWithUTF8String:placementId];
+    if (!unityShowDelegate) {
+        unityShowDelegate = [[DefUnityShowDelegate alloc] init];
+    }
+    [UnityAds show:uiViewController placementId:placementId_s showDelegate:unityShowDelegate];
+}
+
+void SetDebugMode(bool is_debug) {
     BOOL enableDebugMode = is_debug ? YES : NO;
     [UnityAds setDebugMode:enableDebugMode];
 }
 
-bool isReady(char* placementId) {
+bool IsReady(char* placementId) {
     BOOL status;
-    if ((placementId != NULL) && (placementId[0] == '\0')) {
-        status = [UnityAds isReady];
-    }
-    else {
-        NSString* placementId_s = [NSString stringWithUTF8String:placementId];
-        status = [UnityAds isReady:placementId_s];
-    }
+    NSString* placementId_s = [NSString stringWithUTF8String:placementId];
+    status = [UnityAds isReady:placementId_s];
     return status == YES;
 }
 
-bool isSupported() {
+bool IsSupported() {
     BOOL status = [UnityAds isSupported];
     return status == YES;
 }
 
-bool isInitialized() {
+bool IsInitialized() {
     BOOL status = [UnityAds isInitialized];
     return status == YES;
 }
 
-bool getDebugMode() {
+bool GetDebugMode() {
     BOOL status = [UnityAds getDebugMode];
     return status == YES;
 }
 
-char const* getVersion() {
+char const* GetVersion() {
     NSString *version = [UnityAds getVersion];
     const char *version_lua = [version UTF8String];
     return version_lua;
 }
 
-int getPlacementState(char* placementId) {
+int GetPlacementState(char* placementId) {
     UnityAdsPlacementState state;
     if ((placementId != NULL) && (placementId[0] == '\0')) {
         state =[UnityAds getPlacementState];
@@ -146,9 +184,12 @@ int getPlacementState(char* placementId) {
 }
 
 //Banner:
+
+static UADSBannerView *gDefVideoAdsBannerView;
 static NSMutableArray *constraints;
 static DefUnityBannerPosition currentPosition;
 static bool isBannerVisible;
+static DefUnityAdsBannerDelegate* unityBannerDelegate;
 
 static void ApplyBannerPosition() {
     if (gDefVideoAdsBannerView){
@@ -172,8 +213,6 @@ static void ApplyBannerPosition() {
             case BANNER_POSITION_BOTTOM_RIGHT:
             case BANNER_POSITION_TOP_RIGHT:
                 [constraints addObject:[NSLayoutConstraint constraintWithItem:gDefVideoAdsBannerView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:uiViewController.view attribute:NSLayoutAttributeRight multiplier:1.0 constant:0]];
-                break;
-            case kUnityAdsBannerPositionNone:
                 break;
         }
         // position vertically
@@ -208,24 +247,27 @@ static void ApplyBannerPosition() {
     }
 }
 
-void loadBanner(char* placementId, int width, int height) {
+void LoadBanner(char* placementId, int width, int height) {
     if (!gDefVideoAdsBannerView){
         NSString* placementId_s = [NSString stringWithUTF8String:placementId];
         UADSBannerView *localBannerView = [[UADSBannerView alloc] initWithPlacementId: placementId_s size: CGSizeMake(width, height)];
-        localBannerView.delegate = [[DefUnityAdsBannerDelegate alloc] init];
+        if (!unityBannerDelegate) {
+            unityBannerDelegate = [[DefUnityAdsBannerDelegate alloc] init];
+        }
+        localBannerView.delegate = unityBannerDelegate;
         [localBannerView load];
     }
 }
 
-void unloadBanner() {
-    hideBanner();
+void UnloadBanner() {
+    HideBanner();
     if (gDefVideoAdsBannerView){
         [gDefVideoAdsBannerView release];
         gDefVideoAdsBannerView = nil;
     }
 }
 
-void showBanner() {
+void ShowBanner() {
     if (gDefVideoAdsBannerView){
         isBannerVisible = true;
         gDefVideoAdsBannerView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -234,38 +276,199 @@ void showBanner() {
     }
 }
 
-void hideBanner() {
+void HideBanner() {
     if (gDefVideoAdsBannerView) {
         isBannerVisible = false;
         [gDefVideoAdsBannerView removeFromSuperview];
     }
 }
 
-void setBannerPosition(DefUnityBannerPosition bannerPosition) {
+void SetBannerPosition(DefUnityBannerPosition bannerPosition) {
     currentPosition = bannerPosition;
     if (isBannerVisible) {
         ApplyBannerPosition();
     }
 }
 
-void InitExtension() {
+void Initialize_Ext() {
     isBannerVisible = false;
-    setBannerPosition(BANNER_POSITION_TOP_CENTER);
+    SetBannerPosition(BANNER_POSITION_TOP_CENTER);
 }
 
-void FinalizeExtension() {
+void Finalize_Ext() {
     if (constraints){
         [uiViewController.view removeConstraints:constraints];
         [constraints release];
         constraints = nil;
     }
     if (gDefVideoAdsBannerView) {
-        hideBanner();
+        HideBanner();
         [gDefVideoAdsBannerView release];
         gDefVideoAdsBannerView = nil;
     }
 }
 
 } //namespace
+
+@implementation DefUnityAdsBannerDelegate
+- (void)bannerViewDidLoad:(UADSBannerView *)bannerView {
+    dmUnityAds::gDefVideoAdsBannerView = bannerView;
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_BANNER, dmUnityAds::EVENT_LOADED, @"placement_id", bannerView.placementId);
+}
+
+- (void)bannerViewDidClick:(UADSBannerView *)bannerView {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_BANNER, dmUnityAds::EVENT_CLICKED, @"placement_id", bannerView.placementId);
+}
+
+- (void)bannerViewDidLeaveApplication:(UADSBannerView *)bannerView {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_BANNER, dmUnityAds::EVENT_LEFT_APPLICATION, @"placement_id", bannerView.placementId);
+}
+
+- (void)bannerViewDidError:(UADSBannerView *)bannerView error:(UADSBannerError *)error{
+    int code;
+    switch (error.code) {
+        case UADSBannerErrorCodeUnknown:
+            code = dmUnityAds::ERROR_UNKNOWN;
+            break;
+        case UADSBannerErrorCodeNativeError:
+            code = dmUnityAds::ERROR_NATIVE;
+            break;
+        case UADSBannerErrorCodeWebViewError:
+            code = dmUnityAds::ERROR_WEBVIEW;
+            break;
+        case UADSBannerErrorCodeNoFillError:
+            code = dmUnityAds::ERROR_NO_FILL;
+            break;
+    }
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_BANNER, dmUnityAds::EVENT_SDK_ERROR, @"code", code, @"error", error.localizedDescription);
+}
+@end
+
+@implementation DefUnityAdsDelegate
+
+-(void)unityAdsReady:(NSString *)placementId {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_LOAD, dmUnityAds::EVENT_LOADED, @"placementId", placementId);
+}
+
+-(void)unityAdsDidStart:(NSString *)placementId {
+    // this logic is in UnityAdsShowDelegate
+}
+
+-(void)unityAdsDidError:(UnityAdsError)error withMessage:(NSString *)message {
+    // this logic is in UnityAdsShowDelegate
+}
+
+-(void)unityAdsDidFinish:(NSString *)placementId withFinishState:(UnityAdsFinishState)state {
+    // this logic is in UnityAdsShowDelegate
+}
+
+@end
+
+@implementation DefUnityAdsInitializationDelegate
+
+-(void)initializationComplete {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_INIT, dmUnityAds::EVENT_COMPLETED);
+}
+
+-(void)initializationFailed:(UnityAdsInitializationError)error withMessage:(NSString *)message {
+    int code;
+    switch (error) {
+        case kUnityInitializationErrorInternalError:
+            code = dmUnityAds::ERROR_INTERNAL;
+            break;
+        case kUnityInitializationErrorInvalidArgument:
+            code = dmUnityAds::ERROR_INVALID_ARGUMENT;
+            break;
+        case kUnityInitializationErrorAdBlockerDetected:
+            code = dmUnityAds::ERROR_AD_BLOCKER_DETECTED;
+            break;
+    }
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_INIT, dmUnityAds::EVENT_SDK_ERROR, @"code", code, @"error", message);
+}
+
+@end
+
+@implementation DefUnityShowDelegate
+
+-(void)unityAdsShowFailed:(NSString *)placementId withError:(UnityAdsShowError)error  withMessage:(NSString *)message {
+    int code;
+    switch (error) {
+        case kUnityShowErrorNotInitialized:
+            code = dmUnityAds::ERROR_NOT_INITIALIZED;
+            break;
+        case kUnityShowErrorNotReady:
+            code = dmUnityAds::ERROR_NOT_READY;
+            break;
+        case kUnityShowErrorVideoPlayerError:
+            code = dmUnityAds::ERROR_VIDEO_PLAYER;
+            break;
+        case kUnityShowErrorInvalidArgument:
+            code = dmUnityAds::ERROR_INVALID_ARGUMENT;
+            break;
+        case kUnityShowErrorNoConnection:
+            code = dmUnityAds::ERROR_NO_CONNECTION;
+            break;
+        case kUnityShowErrorAlreadyShowing:
+            code = dmUnityAds::ERROR_ALREADY_SHOWING;
+            break;
+        case kUnityShowErrorInternalError:
+            code = dmUnityAds::ERROR_INTERNAL;
+            break;
+    }
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_SHOW, dmUnityAds::EVENT_SDK_ERROR, @"code", code, @"error", message);
+}
+
+-(void)unityAdsShowStart:(NSString *)placementId {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_SHOW, dmUnityAds::EVENT_START, @"placementId", placementId);
+}
+
+-(void)unityAdsShowClick:(NSString *)placementId {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_SHOW, dmUnityAds::EVENT_CLICKED, @"placementId", placementId);
+}
+
+-(void)unityAdsShowComplete:(NSString *)placementId  withFinishState:(UnityAdsShowCompletionState)state {
+    dmUnityAds::MessageEvent event;
+    switch (state) {
+        case kUnityShowCompletionStateSkipped:
+        event = dmUnityAds::EVENT_SKIPPED;
+        break;
+        case kUnityShowCompletionStateCompleted:
+        event = dmUnityAds::EVENT_COMPLETED;
+        break;
+    }
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_SHOW, event, @"placementId", placementId);
+}
+
+@end
+
+@implementation DefUnityAdsLoadDelegate
+
+-(void)unityAdsAdLoaded:(NSString *)placementId {
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_LOAD, dmUnityAds::EVENT_LOADED, @"placementId", placementId);
+}
+
+-(void)unityAdsAdFailedToLoad:(NSString *)placementId withError:(UnityAdsLoadError)error withMessage:(NSString *)message {
+    int code;
+    switch (error) {
+        case kUnityAdsLoadErrorInitializeFailed:
+            code = dmUnityAds::ERROR_NOT_INITIALIZED;
+            break;
+        case kUnityAdsLoadErrorInternal:
+            code = dmUnityAds::ERROR_INTERNAL;
+            break;
+        case kUnityAdsLoadErrorInvalidArgument:
+            code = dmUnityAds::ERROR_INVALID_ARGUMENT;
+            break;
+        case kUnityAdsLoadErrorNoFill:
+            code = dmUnityAds::ERROR_NO_FILL;
+            break;
+        case kUnityAdsLoadErrorTimeout:
+            code = dmUnityAds::ERROR_TIMEOUT;
+            break;
+    }
+    dmUnityAds::SendSimpleMessage(dmUnityAds::MSG_LOAD, dmUnityAds::EVENT_SDK_ERROR, @"code", code, @"error", message);
+}
+
+@end
 
 #endif
